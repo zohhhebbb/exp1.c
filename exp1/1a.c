09dce46c0f60916e7085315cedcb2f7150b13d74
zohhhebbb <sayyadzoheb1@gmail.com>
2025-10-17 00:05:11 +0530

    Update 1a.c

--- exp1/1a.c
+++ exp1/1a.c
@@ -1,18 +1,14 @@
 //Zoheb Sayyad  D(D3) comp 251P123
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    int num;
-    printf("Enter your num : ");
-    scanf("%d", &num);
-    if (num %2 == 0){
-        printf("Even");
-    }
-    
-    else{
-        printf("odd");
-    }
+    printf("Name : Zoheb Samirahamad Sayyad\n");
+    printf("Age : 17\n");
+    printf("Class : Comp engg\n");
+    printf("Div : D(D3)\n");
+    printf("UIN : 251P123\n");
 
-return 0;
+    return 0;
 }
+
